455d64365cc3d82618b07721417ccb982d9c5b31
xuxoel <jesus.carreja@yahoo.com>
2024-07-09 20:48:16 +0200

    Added vim-tmux-navigator plugin

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,19 @@
+local Plugin = {'christoomey/vim-tmux-navigator'}
+
+Plugin.cmd = {
+  "TmuxNavigateLeft",
+  "TmuxNavigateDown",
+  "TmuxNavigateUp",
+  "TmuxNavigateRight",
+  "TmuxNavigatePrevious",
+}
+
+Plugin.keys = {
+  { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+  { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+  { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+  { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+  { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" }
+}
+
+return Plugin
